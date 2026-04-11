573db985b75b5b64efe12fa39dd5b9fe67d857ed
Sushil Jadhav <sushiljadhav407@gmail.com>
2026-04-11 18:53:40 +0530

    added new design

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,7 +1,7 @@
 const Hero = () => {
     return (<section id="home" className="relative bg-[#f6f0e8] ">
       <div className="relative max-h-[180px] w-full md:max-h-[700px]">
-        <img src="/assets/hero-banner.png" alt="Modern co-living space" className="block h-full w-full object-cover"/>
+        <img src="/assets/hero-banner.webp" alt="Modern co-living space" className="block h-full w-full object-cover"/>
 
         
       </div>
